fd31b0504e91e391f6564d3c6e1ff9b46286907e
hans.chicken <hans.chicken@kakaocorp.com>
2023-12-05 11:14:54 +0900

    updated get_token.sh

--- bash-scripts/VM/get_token.sh
+++ bash-scripts/VM/get_token.sh
@@ -9,8 +9,8 @@ TOKEN=$(curl -s -X POST -i https://iam.kakaoi.io/identity/v3/auth/tokens -H "Con
                 "application_credential"
             ],
             "application_credential": {
-                "id": "'60178ebe559b4875bc0ef5f55b3b60af'",
-                "secret": "'GQqvm10mfViScBVq3M9ps5oj-mKPYhjuqOGc6n-kaaBYr3OY1ijmhbgheerb5Y5r_VC_k3ac6U6DmDKgswXgJA'"
+                "id": "'"$ACCESS_KEY"'",       
+                "secret": "'"$ACCESS_SECRET_KEY"'"
             }
         }
     }
